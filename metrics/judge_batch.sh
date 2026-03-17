2a7ed1db87014ce1ade7e2b3ac8fe8bbb92113e7
root <root@tjdm-inf-sci-k8s-a800-00034.tjdm.baidu.com>
2026-03-17 19:27:11 +0800

    judge

--- metrics/judge_batch.sh
+++ metrics/judge_batch.sh
@@ -5,3 +5,8 @@
 for f in api/output/page/*.jsonl; 
     do     base=$(basename "$f");     python metrics/judge.py "$f" "metrics/output_new/page_judged-1/$base";   
 done
+
+
+for f in api/output/pred_crop_answer//*.jsonl; 
+    do     base=$(basename "$f");    python metrics/judge.py "$f" "metrics/output_new/pred_bbox_answer-models/$base";   
+done
